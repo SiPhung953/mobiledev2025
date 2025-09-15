6d72de7fdc2af4b3b3c8e7cb25e9c8c57f7e32ef
SiPhung953 <siphung953@gmail.com>
2025-09-15 10:03:23 +0700

    Practical Work 6

---
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherFragment.java
@@ -0,0 +1,20 @@
+package vn.edu.usth.usthweather;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeatherFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_weather, container, false);
+        view.setBackgroundColor(0xdbfbdb);
+        return view;
+    }
+}
